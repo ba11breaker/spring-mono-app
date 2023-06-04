3222870cabdfcf67bc7becf27b81a62a18f070bc
ba11breaker <15212219417@163.com>
2023-06-04 19:02:42 +1000

    init: boot-starter

---
+++ boot-starter/src/test/java/com/ba11breaker/bootstarter/BootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ba11breaker.bootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
