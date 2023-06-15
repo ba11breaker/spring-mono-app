85754a7bead1ce0fd0b4ab717caed817fb46091b
ba11breaker <15212219417@163.com>
2023-06-15 22:43:08 +1000

    create general-mono proj

---
+++ general-mono/src/main/java/com/ba11breaker/general/App.java
@@ -0,0 +1,13 @@
+package com.ba11breaker.general;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
